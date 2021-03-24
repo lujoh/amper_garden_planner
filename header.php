d5e4f60927935a6991f21141db3e4b8f8c1a50ff
lujoh <lucyjohnson1995@gmail.com>
2021-03-23 21:16:08 -0400

    added header file

---
+++ header.php
@@ -0,0 +1,16 @@
+<header>
+    <div class="title">
+        <img class="logo" src="images/garden-logo.jpg" alt="">
+        <h1>Amper Garden Planner</h1>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="">Location Selection</a></li>
+            <li><a href="">Planting Guide &amp; Calendar</a></li>
+            <li><a href="">Watering Guide</a></li>
+            <li><a href="">Plant Care</a></li>
+            <li><a href="">Harvest Instructions</a></li>
+            <li><a href="">Add your own plants!</a></li>
+        </ul>
+    </nav>
+</header>
