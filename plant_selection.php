3d08c8b27f0822192430b2f4e044cbbc58909c1a
lujoh <lucyjohnson1995@gmail.com>
2021-03-23 21:45:25 -0400

    added plant_selection file

---
+++ plant_selection.php
@@ -0,0 +1,21 @@
+<aside class="plant_selection">
+    <header>
+        <h3>Choose your plants</h3>
+        <button>Hide plant selection</button>
+    </header>
+    <section>
+        <div>
+            <h3>Selected plants:</h3>
+            <ul>
+                <li></li>
+            </ul>
+        </div>
+        <div>
+            <form name="plant_selection">
+                <h3>Add plants:</h3>
+                
+                <input type="submit" value="Submit plant selection">
+            </form>
+        </div>
+    </section>
+</aside>
