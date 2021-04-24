d62f9c5d753b418f21a7638d15f2d5a63297f874
lujoh <lucyjohnson1995@gmail.com>
2021-04-23 23:52:09 -0400

    plant selection query progress

--- page_components/plant_selection.php
+++ page_components/plant_selection.php
@@ -1,11 +1,19 @@
 <aside class="plant_selection">
     <header>
+        
         <button class="hide_button" aria-expanded="true" onclick="toggle_visibility(this, 'selection_hidden', 'Hide plant selection', 'Show plant selection')">Hide plant selection</button>
         <h2>Choose your plants</h2>
     </header>
     <section class="hidden_area visible" id="selection_hidden">
+<!--        Here we insert the file where the plant selection query happens-->
+        <?php
+        include 'plant_selection_query.php';
+        ?>
+<!--        Here we will check to see if the form has been submitted 
+and go over each plant from the query results to see if they have been selected and add them to the your_plants variable-->
         <div>
             <h3>Selected plants:</h3>
+<!--            Here we will go over all the plants in your_plants and create a list item to show that it has been selected-->
             <ul>
                 <li>Nasturtium</li>
                 <li>Basil</li>
@@ -14,13 +22,22 @@
         <div>
             <form name="plant_selection">
                 <h3>Add plants:</h3>
-                <label for="plant_search">Search available plants</label>
-                <select name="plant_search" id="plant_search">
-                    <option value="Basil">Basil</option>
-                    <option value="Tomato">Tomato</option>
-                    <option value="Nasturtium">Nasturtium</option>
-                    <option value="Carrot">Carrot</option>
-                </select>
+
+<!--                This will go over each plant in the results of the plant selection query and reate a checkbox-->
+                <?php
+                while($row = $selection_result->fetch_assoc()){
+                    //checks if the item was previously selected
+                    $checked_status = "";
+                    if (isset($your_plants[$row['p.plant_id']])){
+                        $checked_status = "checked";
+                    }
+                    
+                    //create checkboxes
+                    echo "<input type='checkbox' name='" . $row['p.plant_id'] . "' id='" . $row['p.plant_id'] . "' value='" . $row['p.plant_id'] . "' " . $checked_status . ">";
+                    //create label
+                    echo "<label for='" . $row['p.plant_id'] . "'>" . $row['p.plant_name'] . "</label><br>";
+                }
+                ?>
                 <input type="submit" value="Submit plant selection">
             </form>
         </div>
---
+++ page_components/plant_selection_query.php
@@ -0,0 +1,22 @@
+<?php
+//this file starts an sql query for the plant selection section and saves the result in a variable to be used in the plant selection area
+include 'pwd.php';
+$selection_error = false;
+$conn = new mysqli($servername, $username, $password, $db);
+
+if ($conn->connect_error) {
+    $selection_error = true;
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$selection_sql = 
+    "SELECT p.plant_id, p.plant_name
+    FROM plants p;";
+
+$selection_result = $conn->query($selection_sql);
+if (!$selection_result){
+    $selection_error = true;
+}
+
+$conn->close();
+?>
